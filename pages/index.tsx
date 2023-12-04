573a6d8518364e84cebcd6a919a477fe8cefd37b
Jan Kaifer <jan@kaifer.cz>
2023-12-04 17:05:22 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,30 +6,7 @@ export default function Page() {
       <Head>
         <meta name="og:title" content="Vercel Edge Network" />
         <meta name="og:description" content="Vercel Edge Network" />
-        <meta
-          name="og:image"
-          content={
-            // Because OG images must have a absolute URL, we use the
-            // `VERCEL_URL` environment variable to get the deployment’s URL.
-            // More info:
-            // https://vercel.com/docs/concepts/projects/environment-variables
-            `${
-              process.env.VERCEL_URL ? 'https://' + process.env.VERCEL_URL : ''
-            }/api/vercel`
-          }
-        />
-        <meta
-          name="twitter:image"
-          content={
-            // Because OG images must have a absolute URL, we use the
-            // `VERCEL_URL` environment variable to get the deployment’s URL.
-            // More info:
-            // https://vercel.com/docs/concepts/projects/environment-variables
-            `${
-              process.env.VERCEL_URL ? 'https://' + process.env.VERCEL_URL : ''
-            }/api/vercel`
-          }
-        />
+       <meta property="og:image" content="https://public.bnbstatic.com/image/cms/blog/20231121/22832137-d38a-4202-a26e-76b836e78b45.png" data-shuvi-head="true">
       </Head>
       <h1>A page with Open Graph Image.</h1>
     </div>
