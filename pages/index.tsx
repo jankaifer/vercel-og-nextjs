c6ae0d4137eb0599bb93128e06f2194e82d43605
Jan Kaifer <jan@kaifer.cz>
2023-12-04 17:23:24 +0100

    fix og image

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,10 @@ export default function Page() {
   return (
     <div>
       <Head>
-       <meta property="og:image" content="https://public.bnbstatic.com/image/cms/blog/20231121/22832137-d38a-4202-a26e-76b836e78b45.png" data-shuvi-head="true"/>
+       <meta property="og:image" content={
+        `${
+          process.env.VERCEL_URL ? 'https://' + process.env.VERCEL_URL : ''
+        }/grafana.png" data-shuvi-head="true"/>
       </Head>
       <h1>A page with Open Graph Image.</h1>
     </div>
