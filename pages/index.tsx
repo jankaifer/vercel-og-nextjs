b1ca483c0853d1cd669dd8c71bca7b110fb11f26
Jan Kaifer <jan@kaifer.cz>
2023-12-04 17:10:04 +0100

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,6 @@ export default function Page() {
   return (
     <div>
       <Head>
-        <meta name="og:title" content="Vercel Edge Network" />
-        <meta name="og:description" content="Vercel Edge Network" />
        <meta property="og:image" content="https://public.bnbstatic.com/image/cms/blog/20231121/22832137-d38a-4202-a26e-76b836e78b45.png" data-shuvi-head="true"/>
       </Head>
       <h1>A page with Open Graph Image.</h1>
